65073817ecef0939efada2eb1ca93f2c1a54d7f8
TomKellyGenetics <tomkellygenetics@gmail.com>
2017-06-01 03:52:20 +1200

    Chapter 5 eds: text (minor eds) + figures

--- compile_latex_all.sh
+++ compile_latex_all.sh
@@ -1,4 +1,4 @@
-for file in  20170506_thesis_appendix    20170427_thesis_introduction  20170504_thesis_results_network 20170427_thesis_disc_concl  20170427_thesis_methods 20170506_thesis_results_simulation  20170428_thesis_results_slipt thesis
+for file in `ls *0529*tex thesis.tex | cut -d"." -f1`
 do
     pdflatex -interaction=nonstopmode ${file}.tex
     bibtex $file
